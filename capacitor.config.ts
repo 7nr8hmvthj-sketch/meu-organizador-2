ab72556df43974cc6886c3358d20db97f3643adc
rentamac <rentamac@rentamacs-Mac-mini.local>
2026-06-05 05:40:47 -0700

    Atualiza app iOS para Meus Plantões build 5

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.meuorganizador.app',
-  appName: 'Meu Organizador',
+  appName: 'Meus Plantões',
   webDir: 'dist/public'
 };
 
